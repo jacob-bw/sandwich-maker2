072f21207e8adfed1542adab6307226e3ee92c30
Jacob <wittenberg.jacob@gmail.com>
2019-09-28 10:45:46 -0500

    added cheese array

--- javascripts/components/cheese.js
+++ javascripts/components/cheese.js
@@ -1,1 +1,27 @@
-console.log("cheese");
+const cheeses = [
+    {
+        id: 'cheese1',
+        name: 'american',
+        price: 50
+    },
+    {
+        id: 'cheese2',
+        name: 'swiss',
+        price: 150
+    },
+    {
+        id: 'cheese3',
+        name: 'brie',
+        price: 250
+    },
+    {
+        id: 'cheese4',
+        name: 'pimento',
+        price: 100
+    },
+    {
+        id: 'cheese5',
+        name: 'provalone',
+        price: 150
+    },
+];
