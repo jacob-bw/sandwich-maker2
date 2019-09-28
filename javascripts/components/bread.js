6a12bde3d98fa011cdfc64c46c1d98d9838d7eb6
Jacob <wittenberg.jacob@gmail.com>
2019-09-28 10:41:46 -0500

    updated bread array price field

--- javascripts/components/bread.js
+++ javascripts/components/bread.js
@@ -2,26 +2,26 @@ const breads = [
     {
         id: 'bread1',
         name: 'wheat',
-        price: '200'
+        price: 200
     },
     {
         id: 'bread2',
         name: 'white',
-        price: '100'
+        price: 100
     },
     {
         id: 'bread3',
         name: 'rye',
-        price: '200'
+        price: 200
     },
     {
         id: 'bread4',
         name: 'hoagie',
-        price: '150'
+        price: 150
     },
     {
         id: 'bread5',
         name: 'wrap',
-        price: '250'
+        price: 250
     },
 ];
